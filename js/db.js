28accb5fc8e7180cd1fb427e5ff6c50163869899
ahmedali8 <imahmedalibhatti@gmail.com>
2020-11-24 22:34:12 +0500

    offline data through firebase indexeddb

--- js/db.js
+++ js/db.js
@@ -1,3 +1,14 @@
+// enable offline data
+db.enablePersistence().catch((err) => {
+  if (err.code === 'failed-precondition') {
+    // probably multiple tabs open at once
+    console.log('persistence failed');
+  } else if (err.code === 'unimplemented') {
+    // lack of browser support for the feature
+    console.log('persistence not available');
+  }
+});
+
 // real-time listener
 db.collection('recipes').onSnapshot((snapshot) => {
   // console.log(snapshot.docChanges());
