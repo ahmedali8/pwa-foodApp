5eb4c4b9ce7ec9f78992ad770c26ccb003fcee89
ahmedali8 <imahmedalibhatti@gmail.com>
2020-11-24 16:50:17 +0500

    realtime data from firebase, setup

--- sw.js
+++ sw.js
@@ -55,25 +55,25 @@ self.addEventListener('activate', (e) => {
 // fetch events
 self.addEventListener('fetch', (e) => {
   // console.log('fetch event', e);
-  e.respondWith(
-    caches
-      .match(e.request)
-      .then((cacheRes) => {
-        return (
-          cacheRes ||
-          fetch(e.request).then((fetchRes) => {
-            return caches.open(dynamicCacheName).then((cache) => {
-              cache.put(e.request.url, fetchRes.clone());
-              limitCacheSize(dynamicCacheName, 15);
-              return fetchRes;
-            });
-          })
-        );
-      })
-      .catch(() => {
-        if (e.request.url.indexOf('.html') > -1) {
-          return caches.match('/pages/fallback.html');
-        }
-      })
-  );
+  // e.respondWith(
+  //   caches
+  //     .match(e.request)
+  //     .then((cacheRes) => {
+  //       return (
+  //         cacheRes ||
+  //         fetch(e.request).then((fetchRes) => {
+  //           return caches.open(dynamicCacheName).then((cache) => {
+  //             cache.put(e.request.url, fetchRes.clone());
+  //             limitCacheSize(dynamicCacheName, 15);
+  //             return fetchRes;
+  //           });
+  //         })
+  //       );
+  //     })
+  //     .catch(() => {
+  //       if (e.request.url.indexOf('.html') > -1) {
+  //         return caches.match('/pages/fallback.html');
+  //       }
+  //     })
+  // );
 });
